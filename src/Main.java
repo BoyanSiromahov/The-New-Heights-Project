bcc50f1102567222c7aafec9aa351f7c575eb8b0
Shaun Gordon <shaun.g224@gmail.com>
2020-01-27 23:20:46 -0500

    Created csv file and csv reader in main.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO code
-
+public class Main 
+{
+	public List<String[]> csvReader()
+	{ 
+	    String file = "../csv.txt";
+	    List<String[]> content = new ArrayList<>();
+	    try (BufferedReader br = new BufferedReader(new FileReader(file))) 
+	    {
+	        String line = "";
+	        while ((line = br.readLine()) != null) 
+	        {
+	            content.add(line.split(","));
+	        }
+	    } catch (FileNotFoundException e) {
+	    } catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	    return content;
 	}
-
+	
+	public static void main(String[] args) 
+	{
+		List<String[]> elevatorData = new ArrayList<String[]>();
+		Main main = new Main();
+		elevatorData = main.csvReader();
+	}
 }
