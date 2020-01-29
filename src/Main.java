0cdda5c23cb205ff3180348f68b9a04d2289826f
sammytripp <samtripp5@gmail.com>
2020-01-29 18:08:53 -0500

    Initialize Floor class for Floor subsystem

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
--- src/SchedulerSubSystem/Scheduler.java
+++ src/SchedulerSubSystem/Scheduler.java
@@ -1,8 +1,6 @@
 package SchedulerSubSystem;
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
+
 
 public class Scheduler implements Runnable{
 	 private List<String> content;
