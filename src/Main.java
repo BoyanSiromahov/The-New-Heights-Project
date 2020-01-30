0c795ca09b31708b60d0f9e82582b95327fc4f80
Shaun Gordon <shaun.g224@gmail.com>
2020-01-29 23:46:54 -0500

    Improved storage method for csv info with type.

--- src/Main.java
+++ src/Main.java
@@ -10,86 +10,22 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import Util.Parser;
 
 /**
  * The Class Main.
  */
 public class Main 
 {
-	/**
-	 * Reads CSV file into a list of arrays.  
-	 *
-	 * @return the list
-	 * @throws ParseException the parse exception
-	 */
-	public List<String[]> csvReader() throws ParseException
-	{ 
-	    String file = "../csv.txt";
-	    List<String[]> content = new ArrayList<>();
-	    try (BufferedReader br = new BufferedReader(new FileReader(file))) 
-	    {
-	        String line = "";
-	        while ((line = br.readLine()) != null) 
-	        {
-	            content.add(line.split(","));
-	        }
-	    } catch (FileNotFoundException e) {
-	    } catch (IOException e1) {
-			e1.printStackTrace();
-		}
-	    content.remove(0);  //Deletes titles. 
-		return content;
-	}
-	
-	/**
-	 * Converts a list of string arrays to an array of dates (minutes, seconds, milliseconds).  Can be used for parsed info from CSV   
-	 *
-	 * @param list the list
-	 * @return the date[]
-	 * @throws ParseException the parse exception
-	 */
-	public Date[] toDateArray(List<String[]> list) throws ParseException
-	{
-		DateFormat standard = new SimpleDateFormat("mm:ss.SSS");
-		Date date = null;	
-		Date[] array =  new Date[list.size()];
-		
-		
-		for (int i = 0; i<list.size(); i++)
-		{
-			date = standard.parse(list.get(i)[0]);
-			array[i] = date;
-			System.out.println(array[i]);
-		}
-		return array;
-	}
-	
-	/**
-	 * Converts a list of string arrays to an array of Integers.  Can be used for parsed info from CSV   
-	 *
-	 * @param list the list
-	 * @param column the column
-	 * @return the integer[]
-	 */
-	public Integer[] toIntArray(List<String[]> list, int column)
-	{
-		Integer[] array =  new Integer[list.size()];
-		for (int i = 0; i<list.size(); i++)
-		{
-			array[i] = Integer.parseInt(list.get(i)[column]);
-			System.out.println(array[i]);
-		}
-		return array;
-		
-	}
-
 
 	public static void main(String[] args) throws ParseException 
 	{
-		List<String[]> elevatorData = new ArrayList<>();
-		Main main = new Main();
-		elevatorData = main.csvReader();
+		List<String[]> csvContent = new ArrayList<String[]>();
+		List<Parser> elevatorData = new ArrayList<Parser>();
+		Parser parser = new Parser();
+		
+		csvContent = parser.csvReader();
+		elevatorData = parser.makeList(csvContent);
 	}
 }
--- src/Util/Parser.java
+++ src/Util/Parser.java
@@ -4,34 +4,123 @@
 package Util;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
-public class Parser {
-	
+public class Parser 
+{
+	private Date startTime;
+	private int startFloor;
+	private int endFloor;
+	private String direction;
+	public Parser()
+	{
+		startTime = null;
+		startFloor = -1;
+		endFloor = -1;
+		direction = null;
+		
+	}
 
-	public static List<String> csvReader()
+	public static List<String[]> csvReader()
 	{ 
-	    String file = "csv.txt";
-	    List<String> content = new ArrayList<>();
-
-	    BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader(file));
+		String file = "../csv.txt";
+	    List<String[]> content = new ArrayList<>();
+	    
+	    try (BufferedReader br = new BufferedReader(new FileReader(file))) 
+	    {
 	        String line = "";
 	        while ((line = br.readLine()) != null) 
 	        {
-	        	if(!line.contains("Time,")) {
-	        		content.add(line);
-	            }
+	            content.add(line.split(","));
 	        }
-		} catch (IOException e) {
-			System.out.println("fail");
+	    } catch (FileNotFoundException e) {
+	    } catch (IOException e1) {
+			e1.printStackTrace();
 		}
-
+	    content.remove(0);  //Deletes titles.
 	    return content;
 	}
 	
+	
+	public List<Parser> makeList(List<String[]> originaList)
+	{
+		List<Parser> newList = new ArrayList<Parser>();
+		Parser tempParser;
+		int number;
+		
+		DateFormat standard = new SimpleDateFormat("mm:ss.SSS");
+	    Date date = null;
+		for (int i=0; i<originaList.size(); i++)
+	    {
+			tempParser = new Parser();
+	    	for (int j=0; j<originaList.get(i).length; j++)
+	    	{
+	    		if (j==0)
+	    		{		
+	    			try {
+						date = standard.parse((String) originaList.get(i)[j]);
+					} catch (ParseException e) {
+						e.printStackTrace();
+					}
+	    			tempParser.setStartTime(date);
+	    		}
+	    		else if (j==1)
+	    		{
+	    			number = Integer.parseInt(originaList.get(i)[j]);
+	    			tempParser.setStartFloor(number);
+	    		}
+	    		else if (j==2)
+	    		{
+	    			number = Integer.parseInt(originaList.get(i)[j]);
+	    			tempParser.setEndFloor(number);
+	    		}
+	    		else if (j==3)
+	    		{
+	    			tempParser.setDirection(originaList.get(i)[j]);
+	    		}
+	    	}
+	    	newList.add(tempParser);
+	    }
+		return newList;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public int getStartFloor() {
+		return startFloor;
+	}
+
+	public void setStartFloor(int startFloor) {
+		this.startFloor = startFloor;
+	}
+
+	public int getEndFloor() {
+		return endFloor;
+	}
+
+	public void setEndFloor(int endFloor) {
+		this.endFloor = endFloor;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
 }
