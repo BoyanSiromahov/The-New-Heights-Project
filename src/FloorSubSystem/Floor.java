2eb9ca66da8b40a7ebe3a88ea7bd7c52920f0699
Boyan Siromahov <boyan.siromahov@hotmail.com>
2020-01-30 21:24:14 -0500

    added javadocs

--- src/FloorSubSystem/Floor.java
+++ src/FloorSubSystem/Floor.java
@@ -16,7 +16,8 @@ import SchedulerSubSystem.Scheduler;
 import Util.Parser;
 
 /**
- * 
+ * This class represents the floor subsystem. It is in charge of tracking time and send events. 
+ * Events are parsed from the CSV file.
  * 
  * @author Samantha Tripp
  *
@@ -38,21 +39,27 @@ public class Floor implements Runnable {
 		this.floorEvents = floorEvents;
 	}
 
+	
+	/***
+	 * This is the main method that is implemented from the Runnable interface. This
+	 * method ensure that only one floor thread can process the request and
+	 * respond accordingly. (Ensure The Operation is Atomic)
+	 */
 	@Override
 	public void run() {
 
 		long startTime = System.currentTimeMillis() / 1000;
 		long elapsedTime = 0L;
 		while (true) {
-			elapsedTime = (System.currentTimeMillis() / 1000 - startTime);
+			elapsedTime = (System.currentTimeMillis() / 1000 - startTime); //record time since the program started in s
 			if (floorEvents.size() > 0) {
-				for (int i = 0; i < floorEvents.size(); i++) {
+				for (int i = 0; i < floorEvents.size(); i++) { //only send events while the csv queue exists
 					double millis = floorEvents.get(i).getStartTime().getTime() - 3600000 * 5;
 					// System.out.println("Comparing: " + millis/1000 + " and " + elapsedTime);
-					if (millis / 1000 == elapsedTime) {
+					if (millis / 1000 == elapsedTime) { //when time listed in the csv is the same as elapsed sent event
 						System.out.println("Floor sending event to scheduler:\n" + floorEvents.get(i));
 						scheduler.elevatorRequest(floorEvents.get(i));
-						floorEvents.remove(i);
+						floorEvents.remove(i); //remove event from queue
 					}
 				}
 				try {
@@ -61,8 +68,15 @@ public class Floor implements Runnable {
 					e.printStackTrace();
 				}
 			}
-
+			
+			//used to notify when people have boarded the elevator
 			if (eventQ.size() > 0 && (Integer) eventQ.peek() == scheduler.getArrivedFloor()) {
+				try {
+					Thread.sleep(2300);//sleep for the number of s it takes to board
+					//TO DO: add a variable that will indicate the number of people
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				System.out.println("Elevator arrived, people have boarded");
 				eventQ.pop();
 				scheduler.elevatorBoarded();
--- src/SchedulerSubSystem/Scheduler.java
+++ src/SchedulerSubSystem/Scheduler.java
@@ -1,8 +1,12 @@
-/*
- * 
- */
 package SchedulerSubSystem;
 
+/**
+ * The main class for the scheduler. This class is used as an middle man to 
+ * recieve and forward events from the Floor class and the Elevator class. It also
+ * keeps track of all events hit and queues them up accordingly.
+ * 
+ * @author Boyan Siromahov
+ */
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,6 +23,10 @@ public class Scheduler implements Runnable {
 	public Scheduler() {
 	}
 
+	/***
+	 * This function is used to forward the elevator request to the elevator from
+	 * the floor
+	 */
 	public synchronized Parser getEvent() {
 		while (!elevatorRequest) {
 			try {
@@ -34,12 +42,22 @@ public class Scheduler implements Runnable {
 		return (Parser) eventQ.remove();
 	}
 
+	/***
+	 * This function is used to tell the floor which floor the elevator has arrived
+	 * at
+	 * 
+	 * @param floorNum - the floor number that the elevator has reached. 
+	 */
 	public synchronized void elevatorArrived(int floorNum) {
 		arrivedFloor = floorNum;
 		elevatorArrived = true;
 		notifyAll();
 	}
 
+	/***
+	 * This function is used to tell the floor which floor the elevator has arrived
+	 * at
+	 */
 	public synchronized int getArrivedFloor() {
 		while (!elevatorArrived) {
 			try {
@@ -53,6 +71,12 @@ public class Scheduler implements Runnable {
 		return arrivedFloor;
 	}
 
+	/***
+	 * This function is used to notify the scheduler to flip the boarded flag to
+	 * true which will then allow the elevator to move
+	 * 
+	 * @param p - the elevator request that is sent from the floor.
+	 */
 	public synchronized void elevatorRequest(Parser p) {
 		System.out.println("Request recieved");
 		elevatorRequest = true;
@@ -60,11 +84,19 @@ public class Scheduler implements Runnable {
 		notifyAll();
 	}
 
+	/***
+	 * This function is used to notify the scheduler to flip the boarded flag to
+	 * true which will then allow the elevator to move
+	 */
 	public synchronized void elevatorBoarded() {
 		elevatorBoarded = true;
 		notifyAll();
 	}
 
+	/***
+	 * This function is used to notify that the elevator can start moving once all
+	 * passengers are boarded. This is sent from floor.
+	 */
 	public synchronized void elevatorReady() {
 		while (!elevatorBoarded) {
 			try {
@@ -76,6 +108,13 @@ public class Scheduler implements Runnable {
 		elevatorBoarded = false;
 	}
 
+	/***
+	 * This is the main method that is implemented from the Runnable interface. This
+	 * method ensure that only one scheduler thread can process the request and
+	 * respond accordingly. (Ensure The Operation is Atomic)
+	 * 
+	 * Currently not in use, will be used in later iterations.
+	 */
 	@Override
 	public void run() {
 
