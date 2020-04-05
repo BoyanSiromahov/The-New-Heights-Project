52b421e36d0f8b4d86a9472ecba8711f119ff71c
thatSoftwareGirl <43473658+thatSoftwareGirl@users.noreply.github.com>
2020-04-05 14:49:48 -0400

    Updated Parser

--- src/Util/Parser.java
+++ src/Util/Parser.java
@@ -13,133 +13,177 @@ import ElevatorSubSystem.Direction;
 
 /**
  *
- * The Parser Class that is responsible for reading the input file and creates the list of callEvents for the elevator.  
+ * The Parser Class that is responsible for reading the input file and creates the list of callEvents for the elevator.
  * @author Shaun Gordon
  */
-public class Parser 
+public class Parser
 {
-	private SimpleDateFormat standard = new SimpleDateFormat("HH:mm:ss"); // Hours/Minutes/seconds/milliseconds
+    private SimpleDateFormat standard = new SimpleDateFormat("HH:mm:ss"); // Hours/Minutes/seconds/milliseconds
     public List<String> systemAddresses;
-	
-	/**
-	 * The function reads in the input data and processes it into a List to be sent
-	 * to the Floor Class
-	 * @return List, the processed csv file data.
-	 */
-	public static List<String[]> csvReader() {
 
-		String file = String.format("csv.txt");
-		List<String[]> content = new ArrayList<>();
+    /**
+     * The function reads in the input data and processes it into a List to be sent
+     * to the Floor Class
+     * @return List, the processed csv file data.
+     */
+    public static List<String[]> csvReader() {
 
-		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			String line = "";
-			while ((line = br.readLine()) != null) {
-				content.add(line.split(","));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		content.remove(0); // Deletes title.
-		return content;
-	}
+        String file = String.format("csv.txt");
+        List<String[]> content = new ArrayList<>();
 
-	/**
-	 * The main function that further process the input parsed data in Floor class compatible
-	 * instance types into a List of CallEvent Object.
-	 * @param originaList, The original processed data.
-	 * @return CallEvent List, The Parser object that is populated from the input data.
-	 */
-	public List<CallEvent> makeList(List<String[]> originaList) {
-		List<CallEvent> newList = new ArrayList<CallEvent>();
-		CallEvent tempParser;
-		int startFloor = 0;
-		int endFloor = 0;
-		Direction direction = null;
-		Faults fault = null;
-		Date date = null;
-		
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        content.remove(0); // Deletes title.
+        return content;
+    }
 
-		// Changing data to proper types
-		for (int i = 0; i < originaList.size(); i++) {
-			//
-			for (int j = 0; j < originaList.get(i).length; j++) {
-				if (j == 0) {
-					try {
+    /**
+     * The function reads in the test data and processes it into a List to be sent
+     * to the Floor Class
+     * @return List, the processed csv file data.
+     */
+    public static List<String[]> softFaultReader() {
 
-						date = standard.parse((String) originaList.get(i)[j]);
+        String file = String.format("soft_fault_csv.txt");
+        List<String[]> content = new ArrayList<>();
 
-					} catch (ParseException e) {
-						e.printStackTrace();
-					}
-				} else if (j == 1) {
-					startFloor = Integer.parseInt(originaList.get(i)[j]);
-				} else if (j == 2) {
-					endFloor = Integer.parseInt(originaList.get(i)[j]);
-				} else if (j == 3) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        content.remove(0); // Deletes title.
+        return content;
+    }
 
-					// Possibility of specification of Fault/Direction
-					if(startFloor == -1 || startFloor == -2){
-						fault = Faults.valueOf(originaList.get(i)[j]);
-					}else {
-						direction = Direction.valueOf(originaList.get(i)[j]);
-					}
+    /**
+     * The function reads in the test data and processes it into a List to be sent
+     * to the Floor Class
+     * @return List, the processed csv file data.
+     */
+    public static List<String[]> hardFaultReader() {
 
-				}
-			}
-			tempParser = new CallEvent(date, startFloor, endFloor, direction, fault);
-			newList.add(tempParser);
-		}
-		return newList;
-	}
+        String file = String.format("hard_fault_csv.txt");
+        List<String[]> content = new ArrayList<>();
 
-	/**
-	 * Parses byte array data received in a DatagramPacket to construct a CallEvent object.
-	 * 
-	 * @param b, The byte array message to decode.
-	 * @return CallEvent, The Parser object that is populated from the input data.
-	 * @throws ParseException 
-	 */
-	public CallEvent parseByteEvent(byte[] b) {
-		
-		//String s = "" + (b[0] & 0xff + b[1] & 0xff);
-		
-		CallEvent tempParser;
-		int startFloor = 0;
-		int endFloor = 0;
-		Direction direction = null;
-		Faults systemFault = null;
-		Date date = null;
-		
-		String s = new String(b, 0, b.length);
-		System.out.println("Request received: " + s );
-		
-		// Parse string with delimiter
-		Scanner scanner = new Scanner(s);
-		scanner.useDelimiter(",");
-	
-		try {
-			// Find next string matching HH:mm:ss to create Date object
-			date = standard.parse((String) scanner.findInLine("[0-9]{2}:[0-9]{2}:[0-9]{2}"));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		scanner.next();
-		startFloor = Integer.parseInt(scanner.next());
-		endFloor = Integer.parseInt(scanner.next());
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                content.add(line.split(","));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        content.remove(0); // Deletes title.
+        return content;
+    }
 
-		// Determining Faults Via The CSV File
-		if(startFloor == -1 || startFloor == -2){
-			systemFault = Faults.valueOf(scanner.next().trim());
-		}else{
-			direction = Direction.valueOf(scanner.next().trim());
-		}
+    /**
+     * The main function that further process the input parsed data in Floor class compatible
+     * instance types into a List of CallEvent Object.
+     * @param originaList, The original processed data.
+     * @return CallEvent List, The Parser object that is populated from the input data.
+     */
+    public List<CallEvent> makeList(List<String[]> originaList) {
+        List<CallEvent> newList = new ArrayList<CallEvent>();
+        CallEvent tempParser;
+        int startFloor = 0;
+        int endFloor = 0;
+        Direction direction = null;
+        Faults fault = null;
+        Date date = null;
 
-		scanner.close();
-		
-		tempParser = new CallEvent(date, startFloor, endFloor, direction, systemFault);
-		
-		return tempParser;
-	}
+
+        // Changing data to proper types
+        for (int i = 0; i < originaList.size(); i++) {
+            //
+            for (int j = 0; j < originaList.get(i).length; j++) {
+                if (j == 0) {
+                    try {
+
+                        date = standard.parse((String) originaList.get(i)[j]);
+
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                } else if (j == 1) {
+                    startFloor = Integer.parseInt(originaList.get(i)[j]);
+                } else if (j == 2) {
+                    endFloor = Integer.parseInt(originaList.get(i)[j]);
+                } else if (j == 3) {
+
+                    // Possibility of specification of Fault/Direction
+                    if(startFloor == -1 || startFloor == -2){
+                        fault = Faults.valueOf(originaList.get(i)[j]);
+                    }else {
+                        direction = Direction.valueOf(originaList.get(i)[j]);
+                    }
+
+                }
+            }
+            tempParser = new CallEvent(date, startFloor, endFloor, direction, fault);
+            newList.add(tempParser);
+        }
+        return newList;
+    }
+
+    /**
+     * Parses byte array data received in a DatagramPacket to construct a CallEvent object.
+     *
+     * @param b, The byte array message to decode.
+     * @return CallEvent, The Parser object that is populated from the input data.
+     * @throws ParseException
+     */
+    public CallEvent parseByteEvent(byte[] b) {
+
+        //String s = "" + (b[0] & 0xff + b[1] & 0xff);
+
+        CallEvent tempParser;
+        int startFloor = 0;
+        int endFloor = 0;
+        Direction direction = null;
+        Faults systemFault = null;
+        Date date = null;
+
+        String s = new String(b, 0, b.length);
+        System.out.println("Request received: " + s );
+
+        // Parse string with delimiter
+        Scanner scanner = new Scanner(s);
+        scanner.useDelimiter(",");
+
+        try {
+            // Find next string matching HH:mm:ss to create Date object
+            date = standard.parse((String) scanner.findInLine("[0-9]{2}:[0-9]{2}:[0-9]{2}"));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        scanner.next();
+        startFloor = Integer.parseInt(scanner.next());
+        endFloor = Integer.parseInt(scanner.next());
+
+        // Determining Faults Via The CSV File
+        if(startFloor == -1 || startFloor == -2){
+            systemFault = Faults.valueOf(scanner.next().trim());
+        }else{
+            direction = Direction.valueOf(scanner.next().trim());
+        }
+
+        scanner.close();
+
+        tempParser = new CallEvent(date, startFloor, endFloor, direction, systemFault);
+
+        return tempParser;
+    }
 
     /**
      * The function reads in the input addresses from the user for the Floor, Scheduler and Elevator
