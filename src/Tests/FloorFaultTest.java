9959321682b0552b33e9ba57444bda36d8fb37ff
thatSoftwareGirl <43473658+thatSoftwareGirl@users.noreply.github.com>
2020-04-05 15:43:13 -0400

    Fault Test cases

---
+++ src/Tests/FloorFaultTest.java
@@ -0,0 +1,34 @@
+package Tests;
+
+import FloorSubSystem.Floor;
+import Util.CallEvent;
+import Util.Parser;
+
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * FloorFaultTest
+ * @author Britney Baker
+ * @author Samantha Tripp
+ */
+
+public class FloorFaultTest {
+    public static void main(String[] args) throws ParseException {
+
+        Parser parser = new Parser();
+        List<CallEvent> elevatorEvents = new ArrayList<CallEvent>();
+        List<String[]> csvData = new ArrayList<String[]>();
+        csvData = Parser.faultReader();
+        elevatorEvents = parser.makeList(csvData);
+
+        Floor f = new Floor(elevatorEvents);
+        try {
+            f.start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Tests/SchedulerFaultTest.java
@@ -0,0 +1,55 @@
+package Tests;
+
+import SchedulerSubSystem.Scheduler;
+
+import java.net.UnknownHostException;
+/**
+ * SchedulerFaultTest
+ * @author Britney Baker
+ * @author Samantha Tripp
+ */
+
+public class SchedulerFaultTest {
+    public static void main(String[] args) throws UnknownHostException {
+
+        Scheduler schedulerControl = new Scheduler();
+
+        //Thread 1 - Communication Link B/w Scheduler & Floor
+        Thread floor_To_Scheduler = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        schedulerControl.elevatorRequest();
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Floor_Scheduler_Communication_Link");
+        floor_To_Scheduler.start();
+
+        //Thread 2 - Communication Link B/w Scheduler & Elevator
+        Thread scheduler_To_Elevator = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    schedulerControl.elevatorStatus();
+                }
+            }
+        }, "Scheduler_Elevator_Communication_Link");
+        scheduler_To_Elevator.start();
+
+        //Thread 3 - Communication Link B/w Scheduler & Elevator For Fault Handling
+        Thread scheduler_Elevator_Fault_Handler = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    schedulerControl.handleElevatorFault();
+                }
+            }
+        }, "scheduler_Elevator_Fault_Handler");
+        scheduler_Elevator_Fault_Handler.start();
+
+    }
+}
